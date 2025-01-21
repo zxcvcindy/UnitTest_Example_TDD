dcbaa65c2e92c1a059659e876abdb50dd69edf86
Josh Hsieh <josh.hsieh@linux.com>
2025-01-21 15:37:36 +0800

    feat: add multiply and divide

--- cal.js
+++ cal.js
@@ -5,3 +5,11 @@ export function add(x, y) {
 export function minus(x, y) {
 	return x - y;
 }
+
+export function multiply(x, y) {
+	return x * y;
+}
+
+export function divide(x, y) {
+	return x / y;
+}
--- tests/cal.test.js
+++ tests/cal.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { add, minus } from "../cal.js";
+import { add, minus, multiply, divide } from "../cal.js";
 
 describe("add function", () => {
 	it("add(1, 2) = 3", () => {
@@ -19,5 +19,25 @@ describe("minus function", () => {
 
 	it("minus(1, -2) = 3", () => {
 		assert.deepStrictEqual(minus(1, -2), 3);
+	});
+});
+
+describe("multiply function", () => {
+	it("multiply(1, 2) = 2", () => {
+		assert.deepStrictEqual(multiply(1, 2), 2);
+	});
+
+	it("multiply(1, -2) = -2", () => {
+		assert.deepStrictEqual(multiply(1, -2), -2);
+	});
+});
+
+describe("divide function", () => {
+	it("divide(1, 2) = 0.5", () => {
+		assert.deepStrictEqual(divide(1, 2), 0.5);
+	});
+
+	it("divide(1, -2) = -0.5", () => {
+		assert.deepStrictEqual(divide(1, -2), -0.5);
 	});
 });
